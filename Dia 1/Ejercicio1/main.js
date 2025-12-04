650fb44a84b8164a2967e935a0aef5987a0b8498
JuanJTorresB <juanjtorresbecerra@gmail.com>
2025-12-04 12:42:33 -0500

    feat: :truck: Folder Renamed

--- Dia 1/Ejercicio1/main.js
+++ Dia 1/Ejercicio1/main.js
@@ -1,5 +1,42 @@
-console.log("Prueba en consola");
+const url = "https://rickandmortyapi.com/api/character";
 
-const doblar =(n)=>{
-  return n*2
-}
+const button_buscar = document.querySelector("#buscar_buttton");
+const search_bar = document.querySelector("#search_bar");
+const card_container = document.querySelector(".cards-container");
+
+// /?page=1
+// /?name=rick
+
+const get_searched_characters = async (search_bar) => {
+  try {
+    res = await fetch(url + `?name=${search_bar}`);
+    data = await res.json();
+    data.results.forEach((char) => {
+      const card = document.createElement("div");
+      card.classList.add("card");
+
+      const img = document.createElement("img");
+      img.src = char.image;
+      img.alt = char.name;
+
+      const h3 = document.createElement("h3");
+      h3.textContent = char.name;
+
+      const p = document.createElement("p");
+      p.textContent = `Estado: ${char.status}`;
+
+      card.append(img, h3, p);
+      card_container.append(card);
+    });
+  } catch (error) {
+    console.error(error);
+    card_container.innerHTML = "<div class='not-found'><h3>Personaje No Encontrado</h3></div>";
+  }
+};
+
+const handle_buscar = () => {
+  card_container.innerHTML = "";
+  get_searched_characters(search_bar.value);
+};
+
+button_buscar.addEventListener("click", handle_buscar);
